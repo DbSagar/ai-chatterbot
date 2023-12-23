3faa5ac3e5c28321b05dedf2ae018c0a08b745bb
sagar shetty <sagardb9#gmail.com>
2023-12-23 18:18:07 +0530

    final

---
+++ components/navbar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Menu, Sparkle } from "lucide-react";
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./mode-toggle";
+import { MobileSidebar } from "./mobile-sidebar";
+
+const font = Poppins({
+  weight: "600",
+  subsets: ["latin"],
+});
+export const Navbar = () => {
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
+      <div className="flex items-center">
+        <MobileSidebar/>
+        <Link href="/">
+          <h1 className={cn(
+            "hidden md:block text-xl md:text-3xl font-bold text-primary",
+            font.className
+            )}>
+            chatterbot.ai
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <ModeToggle/>
+            <UserButton afterSignOutUrl="/"/>
+      </div>
+    </div>
+  );
+};
