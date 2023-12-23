9958662af7690f1173fbfb1bbec12825e0d347d8
sagar shetty <sagardb9#gmail.com>
2023-12-23 18:31:41 +0530

    fix

--- components/companions.tsx
+++ components/companions.tsx
@@ -29,7 +29,7 @@ export const Companions = ({
                 />
                 </div>
                 <p className="text-sm text-muted-foreground">
-                    No ChatterBot's found
+                    No ChatterBot found
                 </p>
             </div>
         )
