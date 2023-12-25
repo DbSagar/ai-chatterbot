735c3e0dea6f50b367d56313c2f6cf43d2efc0a9
sagar shetty <sagardb9#gmail.com>
2023-12-25 21:20:14 +0530

    fix6

--- lib/memory.ts
+++ lib/memory.ts
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { PineconeClient,Pinecone } from "@pinecone-database/pinecone";
+import { PineconeClient } from "@pinecone-database/pinecone";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
 
 export type CompanionKey = {
@@ -33,15 +33,14 @@ export class MemoryManager {
     companionFileName: string
   ) {
     const pineconeClient = <PineconeClient>this.vectorDBClient;
-    const pinecone = new Pinecone();
-    const pineconeIndex = pinecone.Index(
+
+    const pineconeIndex = pineconeClient.Index(
       process.env.PINECONE_INDEX! || ""
     );
-    
 
     const vectorStore = await PineconeStore.fromExistingIndex(
       new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY }),
-      { pineconeIndex }, 
+      { pineconeIndex }
     ); 
 
     const similarDocs = await vectorStore
